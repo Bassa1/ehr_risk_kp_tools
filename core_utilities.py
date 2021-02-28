a6dab91183f030f6066c8f2e10b131ee65c4aa5d
rtroper <rtroper4@gmail.com>
2021-02-27 17:07:41 -0800

    Use def instead of lambda and add post_query function

--- core_utilities.py
+++ core_utilities.py
@@ -26,13 +26,30 @@ additional_name_mappings = {
 # EHR Risk KP endpoint
 risk_kp_url = "https://api.bte.ncats.io/v1/smartapi/1bef5ecbb0b9aee90023ce9faa2c8974/query"
 
-# Utility lambda functions
-create_query = lambda n, e: {"message": {"query_graph": {"nodes": n, "edges": e}}}
-try_get_prop = lambda v, p: '' if not(p in v.keys()) else v[p]
-get_dict_props = lambda d, props: {p:try_get_prop(d, p) for p in props}
-attr_to_dict = lambda attrs: {} if not(isinstance(attrs, list)) else {a['name']: a['value'] for a in attrs}
-get_equivalent_ids = lambda id_list, type_: [v for k, v in [id_.split(':') for id_ in id_list] if (k == type_)]
-get_map_from_columns = lambda df, c1, c2: {k: v for k, v in zip(df[c1], df[c2])}
+
+# Utility for creating a query
+def create_query(n, e): return{"message": {"query_graph": {"nodes": n, "edges": e}}}
+
+
+# Utility for getting a dictionary value if key exists
+def try_get_prop(v, p): return '' if not (p in v.keys()) else v[p]
+
+
+# Utility for getting a list of values if keys exist
+def get_dict_props(d, props): return {p: try_get_prop(d, p) for p in props}
+
+
+# Utility for getting edge attributes as a dictionary
+def attr_to_dict(attrs): return {} if not (isinstance(attrs, list)) else {a['name']: a['value'] for a in attrs}
+
+
+# Utility for getting equivalent identifiers of a specified type
+def get_equivalent_ids(id_list, type_): return [v for k, v in [id_.split(':') for id_ in id_list] if (k == type_)]
+
+
+# Utility for converting two dataframe columns to a dictionary
+def get_map_from_columns(df, c1, c2): return {k: v for k, v in zip(df[c1], df[c2])}
+
 
 # Function to convert node and edge properties to dataframe
 def dict_to_pd(dict_, prop_list, attr_list):
@@ -55,21 +72,23 @@ def dict_to_pd(dict_, prop_list, attr_list):
 
     return pd.DataFrame(data, columns=headers)
 
+
 # Function to add a column with equivalent ids of a specified type
 def add_equivalent_id_columns(df, type_):
     # Make sure type_ is a list of strings
     type_list = type_ if isinstance(type_, list) else [type_]
     type_list = [str(t) for t in type_list]
-    
+
     # Add additional columns with equivalent ids
     for t in type_list:
         # New column header
-        new_col_header = t if not(t in df.columns) else "{}_equivalent".format(t)
+        new_col_header = t if not (t in df.columns) else "{}_equivalent".format(t)
 
         # Get equivalent ids of the specified type and return modified dataframe
         df[new_col_header] = [get_equivalent_ids(id_list, t) for id_list in df['equivalent_identifiers']]
-        
+
     return df
+
 
 def parse_query_results(response, cutoff=0.5):
     # Get query graph
@@ -80,7 +99,7 @@ def parse_query_results(response, cutoff=0.5):
 
     # Get results (node and edge bindings)
     results = response.json()['message']['results']
-    
+
     # Get node properties as a dataframe
     prop_list = ['name', 'category']
     attr_list = ['equivalent_identifiers']
@@ -113,7 +132,20 @@ def parse_query_results(response, cutoff=0.5):
     filter_ = edge_props_df.feature_coefficient > cutoff
     edge_props_df = edge_props_df.loc[filter_]
     edge_props_df.sort_values(
-        by=['subject_name', 'feature_coefficient'], 
+        by=['subject_name', 'feature_coefficient'],
         ascending=[True, False], inplace=True)
-    
+
     return edge_props_df
+
+
+# Function for posting a query to the API
+def post_query(query, endpoint=risk_kp_url, cutoff=0.5):
+    # Post query
+    response = requests.post(endpoint, json=query)
+    assert (response.status_code == 200)
+
+    # Parse results and store in dataframe
+    edge_props_df = parse_query_results(response, cutoff=cutoff)
+
+    return edge_props_df
+
